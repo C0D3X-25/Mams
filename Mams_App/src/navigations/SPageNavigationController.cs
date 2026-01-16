cf5cc75aebe8e1852c2994fbc447058d96c7f628
thjo <jocelin.t9@gmail.com>
2026-01-16 13:49:03 +0100

    Fix: - Missing localization on the navigation

--- Mams_App/src/navigations/SPageNavigationController.cs
+++ Mams_App/src/navigations/SPageNavigationController.cs
@@ -1,4 +1,5 @@
 using Mams_App.src.controllers;
+using Mams_App.src.localizations;
 using Mams_App.src.resumes;
 using System.Windows;
 using System.Windows.Controls;
@@ -67,8 +68,8 @@ public static class SPageNavigationController
             if (_m_current_page?.DataContext is ICompareState compareState && !compareState.isStateOriginal())
             {
                 MessageBoxResult result = MessageBox.Show(
-                    "En quittant la page, toutes les données modifiées seront perdues. Voulez-vous continuer?",
-                    "Annuler",
+                    Loc.Get("Common.UnsavedChangesMessage"),
+                    Loc.Get("Common.Cancel"),
                     MessageBoxButton.YesNo,
                     MessageBoxImage.Warning
                 );
